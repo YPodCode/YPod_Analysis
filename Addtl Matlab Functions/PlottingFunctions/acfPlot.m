a39507d7f419ab2c0063129d63ac288355ce4990
YPodCode <47405270+YPodCode@users.noreply.github.com>
2019-02-09 14:03:47 -0700

    Update acfPlot.m
    Simplify finding sensors for plotting and don't plot at the max screen size every time

--- Addtl Matlab Functions/PlottingFunctions/acfPlot.m
+++ Addtl Matlab Functions/PlottingFunctions/acfPlot.m
@@ -4,51 +4,38 @@ function acfPlot(~,X,Y,~,~,~,settingsSet)
 %Get the time averaging to guess appropriate lags
 deltaT = settingsSet.timeAvg;
 nlags = hours(12)/minutes(deltaT);
+
 %Assume that first column (only column) of Y has data
 yarray = table2array(Y(:,1));
+
 %Remove NaNs
 yarray(isnan(yarray))=0;
+
 %Get pollutant name
 pollutant = settingsSet.fileList.colocation.reference.files.pollutants{settingsSet.loops.i};
 
-nplots = 0;
-for i = 1:size(X,2)
-    %Get variable name
-    VarName = X.Properties.VariableNames{i};
-    %Only plot this for designated sensors (not T, P, Rh, etc)
-    isSensor = 0;
-    for j = 1:length(settingsSet.podSensors)
-        if ~isempty(regexpi(VarName,settingsSet.podSensors{j}))
-            isSensor = 1;
-        end
-    end
-    if isSensor == 1
-        nplots = nplots+1;
-    end
+varNames = X.Properties.VariableNames;
+sensorList = contains(varNames,settingsSet.podSensors,'IgnoreCase',true);
+nplots = sum(sensorList);
+if nplots > 4
+    figure('Position',get( groot, 'Screensize' ));
+else
+    figure('Position',[0 0 600,300*nplots])
 end
 
 plotn = 1;
-figure('Position',get( groot, 'Screensize' ));
 for i = 1:size(X,2)
-    %Get variable name
-    VarName = X.Properties.VariableNames{i};
-    %Get pod name
-    podName = settingsSet.podList.podName{settingsSet.loops.j};
-    
     %Only plot this for designated sensors (not T, P, Rh, etc)
-    isSensor = 0;
-    for j = 1:length(settingsSet.podSensors)
-        if ~isempty(regexpi(VarName,settingsSet.podSensors{j}))
-            isSensor = 1;
-        end
-    end
-    if isSensor == 1
+    if sensorList(i)
+        %Get variable name
+        sensorName = varNames{i};
+        
         %Get X as array
         xarray = table2array(X(:,i));
         %Remove NaNs
         xarray(isnan(xarray))=0;
         
-        %Plot the cross correlation
+        %Plot the cross correlation in a kind of grid
         ncol = round(sqrt(nplots),0);
         subplot(ceil(nplots/ncol),ncol,plotn);
         crosscorr(yarray, xarray,nlags);
@@ -56,10 +43,11 @@ for i = 1:size(X,2)
         %Give the plot a better title
         ylabel('XCF');ylim([-1 1])
         if plotn==1
-            title([pollutant ': ' VarName])
+            title([pollutant ': ' sensorName])
         else
-            title(VarName)
+            title(sensorName)
         end
+        
         %Display the max and min correlations
         [xcf,lags,~] = crosscorr(yarray, xarray,nlags);
         line([0 0],[floor(min(xcf)*10)/10 ceil(max(xcf)*10)/10])
